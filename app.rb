b10867ab45c58244a23fa3c392fe7c554eb4ed0a
Jenise Ruehle <jela07@gmail.com>
2020-10-10 20:11:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,14 +6,7 @@ class App < Sinatra::Base
   end 
   
   post '/team' do 
-    @name = params[:name]
-    @coach = params[:coach]
-    @pg = params[:pg]
-    @sg = params[:sg]
-    @sf = params[:sf]
-    @pf = params[:pf]
-    @c = params[:c]
-    
+    @team = params
     erb :team
   end 
 

39e396b902d078bcb1023f75aeb1a0189b47469c
Jenise Ruehle <jela07@gmail.com>
2020-10-10 19:54:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-  <form 
+  <form method="POST" action="/food">
+    <p>Team Name: <input type="text" name="team_name"></p>
+    <p>Coach: <input type="text" name="coach"></p>
+    
 
 end
